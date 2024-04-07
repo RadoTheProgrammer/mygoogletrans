95df27562867c8987c7be1d841358fa3e216ac9e
RadoTheProgrammer <rado@arazakar.com>
2024-04-07 16:32:04 +0200

    clean up from useless files

--- gtrans_bad.py
+++
@@ -1,7 +0,0 @@
-import googletrans,s
-translator=googletrans.Translator(service_urls=[
-       'translate.google.com',
-
-     ])
-
-txt=translator.translate("Bonjour","en")
--- gtrans_cloud.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import translate_v2 as translat
-def trans(text, target_language, source_language="fr"):
-    translate_client = translate.Client()
-    result = translate_client.translate(text, target_language=target_language, source_language=source_language)
-    return result
-print(trans("Bonjour","Hello"))
